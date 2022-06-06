3f484b4d04bbaf88937a1c941f08437bf0e5fce1
Furry <ender@thefluegges.com>
2022-06-05 21:32:57 -0400

    Improved detection

--- src/index.ts
+++ src/index.ts
@@ -11,11 +11,11 @@ function intersect<T>(a: T[], b: T[]): T[] {
 server.get("/check", (req, res) => {
     console.log(req.headers);
 
-    console.log("remote:", req.socket.remoteAddress);
+    const ip = req.headers["x-forwarded-for"] || (req.connection.remoteAddress || req.socket.remoteAddress);
     let anonymity;
     if (intersect(Object.keys(req.headers).map(x => x.toLowerCase()), ["x-forwarded-for", "x-real-ip", "via", "x-forwarded-for", "x-proxy-id", "from", "proxy-connection", "proxy-authorization"]).length == 0) {
         anonymity = "elite";
-    } else if (!JSON.stringify(req.headers).includes(req.socket.remoteAddress as string)) {
+    } else if (!JSON.stringify(req.headers).includes(ip as string)) {
         anonymity = "anonymous";
     } else {
         anonymity = "transparent";
